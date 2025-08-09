65a903af71fcd6556c2b510272fc4ff28d4ff393
SharifWebDev <sharif.uddin.9977@gmail.com>
2025-08-09 12:09:06 +0600

    test

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -9,12 +9,35 @@ class Authenticate
 {
     public function handle($request, Closure $next, $guard = 'web')
     {
+        dd($guard);
         if (!Auth::guard($guard)->check()) {
-            return $guard === 'customer'
-                ? redirect()->route('customer.login')
-                : redirect()->route('admin.login');
+            if ($guard === 'customer') {
+                return redirect()->route('customer.login')
+                    ->with('error', 'Access Denied! You must login as a customer.');
+            }
+
+            return redirect()->route('admin.login')
+                ->with('error', 'Access Denied! You must login as an admin.');
         }
 
         return $next($request);
+
+        // if (!Auth::guard($guard)->check()) {
+        //     return $guard === 'customer'
+        //         ? redirect()->route('customer.login')
+        //         : redirect()->route('admin.login');
+        // }
+        // return $next($request);
+
+        // if (Auth::guard('admin')->check()) {
+        //     return $next($request);
+        // }
+        // return redirect()->route('admin.login')->with('error', 'Access Denied! You are not an admin.');
+
+        // if (Auth::guard('customer')->check()) {
+        //     return $next($request);
+        // }
+        // return redirect()->route('customer.login')->with('error', 'Access Denied! You are not an customer.');
+
     }
 }
